af4791304090c365bb5cff54ab4736e1fea72197
hitthemoney <50669620+hitthemoney@users.noreply.github.com>
2022-02-17 10:09:10 -0800

    new db

--- js/db.js
+++ js/db.js
@@ -1,11 +1,13 @@
 const PUBLIC_KEY =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzU4NTAzMywiZXhwIjoxOTU5MTYxMDMzfQ.1k98TGa8xrZpYqGwPImvnqtp9rH3rxXbgymp6Xczf5k";
-const URL = "https://xyufqutkefhirssoinju.supabase.co";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtZ25wb3h6bm91eGx0ZGNkY3hlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDUxMjA0MTUsImV4cCI6MTk2MDY5NjQxNX0.9xG_n9LSqtT8Y1KSOrcjBiuDMWWPKzGACl5QJNw2im0";
+const URL = "https://xmgnpoxznouxltdcdcxe.supabase.co";
 
 const { createClient } = supabase;
 const _supabase = createClient(URL, PUBLIC_KEY);
 
 // console.log("Supabase Instance: ", _supabase);
+
+window._supabase = _supabase;
 
 const addToLeaderboard = async (score, time, name = "Guest") => {
     const { data, error } = await _supabase.from("leaderboard").insert([
